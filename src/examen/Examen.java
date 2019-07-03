41e136ccc2ba1366feba9b5a5ecc02dfd923cd88
Federico Bornachera <fedeangel2202@gmail.com>
2019-07-02 23:29:11 -0500

    Modificando algunos errores

--- src/examen/Examen.java
+++ src/examen/Examen.java
@@ -14,28 +14,31 @@ public class Examen {
       ArrayList<Double>Velocidad = new ArrayList<Double>();
       ArrayList<Double>Tiempo = new ArrayList<Double>();
       
-      boolean Conti=true;
-      int Decision=1,promedio=0;
+      boolean Continue=true;
+      int Decision=1;
       String Nombres;
       double velocidad=0, tiempo=0;
       
       tiempo=100/velocidad;
       
-      while(Conti){
+      while(Continue){
           System.out.println("Ingrese el nombre del estudiante ");
           Nombre.add(leer.next());
+          
           System.out.println("");
           System.out.println("Ingrese la velocidad");
           Velocidad.add(leer.nextDouble());
+          
           System.out.println("");
           System.out.println("Ingrese el tiempo tardado");
           Tiempo.add(leer.nextDouble());
+          
           System.out.println("");
           System.out.println("Desea seguir continuando? [1--> Continuar] - [0--> Salir] ");
           Decision=leer.nextInt();
           
           if(Decision==1){
-              Conti=false;
+              Continue=false;
           }
           for(int x=0;x<Nombre.size();x++){
               System.out.println("Listado " + "Nombres: " + Nombre.get(x) + " Velocidad " + " Tiempo" + tiempo );
