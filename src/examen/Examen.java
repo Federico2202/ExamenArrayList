5533b208b44ce52e924b0c6e16d01434517a849f
Federico Bornachera <fedeangel2202@gmail.com>
2019-07-02 23:27:14 -0500

    Iniciando proyecto

---
+++ src/examen/Examen.java
@@ -0,0 +1,47 @@
+
+package examen;
+
+import java.util.*;
+
+public class Examen {
+
+    public static void main(String[] args) {
+
+                
+        Scanner leer = new Scanner(System.in);
+
+      ArrayList<String>Nombre = new ArrayList<String>();
+      ArrayList<Double>Velocidad = new ArrayList<Double>();
+      ArrayList<Double>Tiempo = new ArrayList<Double>();
+      
+      boolean Conti=true;
+      int Decision=1,promedio=0;
+      String Nombres;
+      double velocidad=0, tiempo=0;
+      
+      tiempo=100/velocidad;
+      
+      while(Conti){
+          System.out.println("Ingrese el nombre del estudiante ");
+          Nombre.add(leer.next());
+          System.out.println("");
+          System.out.println("Ingrese la velocidad");
+          Velocidad.add(leer.nextDouble());
+          System.out.println("");
+          System.out.println("Ingrese el tiempo tardado");
+          Tiempo.add(leer.nextDouble());
+          System.out.println("");
+          System.out.println("Desea seguir continuando? [1--> Continuar] - [0--> Salir] ");
+          Decision=leer.nextInt();
+          
+          if(Decision==1){
+              Conti=false;
+          }
+          for(int x=0;x<Nombre.size();x++){
+              System.out.println("Listado " + "Nombres: " + Nombre.get(x) + " Velocidad " + " Tiempo" + tiempo );
+          }
+      }
+        
+    }
+    
+}
